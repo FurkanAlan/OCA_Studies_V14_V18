97cdf174129bff721a43182cca2655ed578a5b48
FurkanAlan <ulkirr@gmail.com>
2020-06-29 12:36:56 -0500

    Java Questions V14-V17-V18

--- src/OCA_Hepsi/V18/OCA223_Q1_60/q018/Car.java
+++ src/OCA_Hepsi/V18/OCA223_Q1_60/q018/Car.java
@@ -13,12 +13,15 @@ public class Car extends Vehicle {
 	String trans;
 	
 	Car(String trans){				// line n1
+//		super();
 		this.trans = trans;
 	}
 	Car(String type, int maxSpeed, String trans){
 		super(type, maxSpeed);	// line n2
 		this.trans =trans;
 	}
+
+
 	public static void main(String[] args) {
 		Car c1 = new Car("Auto");
 		Car c2 = new Car("4W",  150, "Manual")	;
