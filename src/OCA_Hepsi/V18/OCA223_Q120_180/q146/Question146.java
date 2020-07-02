01cad940945881e2676a974b8dfcc7882a83390b
FurkanAlan <ulkirr@gmail.com>
2020-07-02 15:48:32 -0500

    Java Questions V14-V17-V18

--- src/OCA_Hepsi/V18/OCA223_Q120_180/q146/Question146.java
+++ src/OCA_Hepsi/V18/OCA223_Q120_180/q146/Question146.java
@@ -4,36 +4,41 @@ import java.util.Arrays;
 
 public class Question146 {
 
-	public static void main(String[] args) {
-		// Given the code fragment
-		String[] strs=new String[2];
-		System.out.println(strs.length);
-		System.out.println(Arrays.toString(strs));
-		int idx=0;
+    public static void main(String[] args) {
+        // Given the code fragment
+        String[] strs = new String[2];
+        System.out.println(strs);
+        System.out.println(strs.length);
+        System.out.println(Arrays.toString(strs));
+
+        int idx = 0;
 //		strs[0] = "A";
-		System.out.println(strs[0].concat("element "));
-		//we can not concat if array is null
+//        System.out.println(strs[0].concat("element ")); //java.lang.NullPointerException
+        //we can not concat if array is null
 
 		for (String s : strs) {
-			strs[idx].concat("element "+idx);
+			strs[idx].concat("element "+idx);  //java.lang.NullPointerException
 			idx++;
+//Unless otherwise noted, passing a <tt>null</tt> argument to a constructor
+// or method in this class will cause a  NullPointerException to be
+// thrown.
 		}
 
-		for (idx = 0; idx < strs.length; idx++) {
-			System.out.println(strs[idx]);
-			
-			//Answer: D
-			
-		}
-		/*
-		 * What is the output?
-		 * A. Element 0Element 1
-		 * B. Null element 0Nullelement 1
-		 * C. NullNull
-		 * D. A null pointer exception is thrown at runtime.
-		 */
+        for (idx = 0; idx < strs.length; idx++) {
+            System.out.println(strs[idx]);
 
-	}
+            //Answer: D
+
+        }
+        /*
+         * What is the output?
+         * A. Element 0Element 1
+         * B. Null element 0Nullelement 1
+         * C. NullNull
+         * D. A null pointer exception is thrown at runtime.
+         */
+
+    }
 
 }
 
--- src/OCA_Hepsi/V18/OCA223_Q120_180/q147/Question147.java
+++ src/OCA_Hepsi/V18/OCA223_Q120_180/q147/Question147.java
@@ -1,5 +1,70 @@
 package OCA_Hepsi.V18.OCA223_Q120_180.q147;
 
 public class Question147 {
+}
 
+//Burada setter ve getter, this ile ayni isi yapiyor
+class Employee {
+    private String name;
+    private int age;
+    private int salary;
+
+    public Employee() { //main de tanimli olan e1 objesinin constructor u bos oldugu icin burada da bos bir
+        // constructor olmali diger turlu hem Employee de hemde Test class da compile hatasi verir
+    }
+
+    public Employee(String name, int age) {
+        setName(name);
+//        this.name = name;
+        setAge(age);
+//        this.age = age;
+        setSalary(2000);
+//        this.salary = 2000;
+    }
+
+    public Employee(String name, int age, int salary) {
+        this(name, age);
+        setSalary(salary);
+//        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    //eger burada bu metod olmasaydi biz private olan degere ulasamazdik,
+    public void printDetails() {
+        System.out.println(name + " : " + age + " : " + salary);
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        Employee e2 = new Employee("Jack", 50);
+        Employee e3 = new Employee("Chloe", 40, 5000);
+        e1.printDetails();
+        e2.printDetails();
+        e3.printDetails();
+    }
 }
